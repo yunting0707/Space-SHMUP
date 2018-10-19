2956cc9975c83d1189138ced8a5aaeea60b04d34
yunting0707 <yunting0707@users.noreply.github.com>
2018-10-18 21:32:26 -0400

    stop at tags

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;     
+    public float fireRate = 0.3f;  
+    public float health = 10;
+    public int score = 100;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {  
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    public Vector3 pos
+    {     
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+        Move();
+
+        if (bndCheck != null && bndCheck.offDown)
+        {
+                Destroy(gameObject);
+        }
+    }
+
+    public virtual void Move()
+    {  
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
---
+++ Assets/_Scripts/Hero.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+    static public Hero S;
+
+    [Header("Set in Inspector")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Set Dynamically")]
+    public float shieldLevel = 1;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this; 
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+
+
+
+
+
+    void Start () {
+		
+	}
+	
+	
+
+	void Update () {
+        float xAxis = Input.GetAxis("Horizontal");                            
+        float yAxis = Input.GetAxis("Vertical");                           
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+                  
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
